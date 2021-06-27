141e27852e412a9e7931054cca03ba6abc140d80
nadia <ntariqexpt@gmail.com>
2021-06-27 04:05:06 +0000

    userlist fixed

--- src/pages/Users/UsersList.js
+++ src/pages/Users/UsersList.js
@@ -8,57 +8,48 @@ export default class UsersList extends React.Component {
     render() {
         return(
             <Paper elevation={6}>
-                <form >
+                <form>
                     <div className="text-xs-center pb-xs">
                         <div className={"form"} style={{background: 'linear-gradient( lightgrey, white)'}}>
                             <h2 style={{textAlign:"center"}}>Form Validation</h2>
                         </div>
                         <div>
+                            <Card>
+                                <div style={{display: "flex",justifyContent: "space-evenly", padding:" 40px"}}>
+                                    <CardContent style={{
+                                        display: 'flex',
+                                        alignItems: 'center',
+                                        justifyContent: "space-evenly",
+                                        flex: '1 0 auto',
+                                        margin: "normal",
+                                        overflow: 'visible',
+                                        padding:"70px"}}>
+                                        <input type="text"
+                                               placeholder="FirstName"
+                                               id="firstName"
+                                               label="firstName"
+                                               className="Programs"
+                                               margine="normal"/>
+                                        <input type="text"
+                                               placeholder="LastName"
+                                               id="LastName"
+                                               label="LastName"
+                                               className="Programs"
+                                               margine="normal"/>
+                                        <DatePickers/>
+                                        <input type="password" placeholder= "Password"/>
 
-                            <Card style={{padding:"50px", backgroundColor:"#1D2228"}}>
-                                <div className="text-xs-center pb-xs">
+                                        <Button
+                                            style={{color:"#FB8122", backgroundColor:'#E1E2E2'}}
+                                            variant="raised"
+                                            type="submit">Delete</Button>
+                                        <Button
+                                            style={{color:"#FB8122", backgroundColor:'#E1E2E2',justifyContent: "space-evenly"}}
+                                            variant="raised"
+                                            type="submit">Save</Button>
+                                    </CardContent>
+
                                 </div>
-
-                                    <Formik>
-
-                                        <Card style={{
-                                            display: 'flex',
-                                            alignItems: 'center',
-                                            justifyContent: "space-evenly",
-                                            flex: '1 0 auto',
-                                            flexDirection: 'column',
-                                            minHeight: '100%',
-                                            textAlign: 'center',
-                                            margin: "normal",
-                                            overflow: 'visible',
-                                            padding:"70px"}}>
-
-
-                                            <input type="text"
-                                                   placeholder="FirstName"
-                                                   id="firstName"
-                                                   label="firstName"
-                                                   className="Programs"
-                                                   margine="normal"/>
-                                            <input type="text"
-                                                   placeholder="LastName"
-                                                   id="LastName"
-                                                   label="LastName"
-                                                   className="Programs"
-                                                   margine="normal"/>
-                                            <DatePickers/>
-
-                                            <Button
-                                                style={{color:"#FB8122", backgroundColor:'#E1E2E2'}}
-                                                variant="raised"
-                                                type="submit">Delete</Button>
-                                            <Button
-                                                style={{color:"#FB8122", backgroundColor:'#E1E2E2'}}
-                                                variant="raised"
-                                                type="submit">Save</Button>
-                                        </Card>
-
-                                    </Formik>
                             </Card>
                         </div>
                     </div>
